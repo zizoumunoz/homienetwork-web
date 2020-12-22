61cbdafc02a8c1d546f1f3b8001cc0c4e7c239b9
zizoumunoz <zmunoz@outlook.es>
2020-12-21 22:20:35 -0800

    Inital Commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$.getJSON('https://api.mcsrvstat.us/2/mc.homienetwork.ca', function(data) {
+        
+        var playerNumber = `${data.players.online}`;
+        var serverVersion = `${data.version}`;
+        var onlineStatus = `${data.online}`;
+        
+        $("#playerno").html(playerNumber);
+        $("#serverversion").html(serverVersion);
+        if (onlineStatus == 'true'){
+            $("#serverstatus").html('<b style="color: #16aa2f;">Online</b>')
+        } else {
+            $("#serverstatus").html('Offline')
+        }
+    });
