80233d57ad1125e06857f9938283c8b12553e4f4
zizou <zmunoz@outlook.es>
2022-09-25 18:16:46 -0400

    Added copyToClipboard function

--- js/script.js
+++ js/script.js
@@ -12,3 +12,14 @@ $.getJSON('https://api.mcsrvstat.us/2/mc.homienetwork.ca', function(data) {
             $("#serverstatus").html('Offline')
         }
     });
+
+function copyToClipboard()
+{
+
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText("mc.homienetwork.ca");
+
+    // Alert the copied text
+    alert("Copied: " + copyText.value);
+}
