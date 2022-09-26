70d82263a61844a42f4ced18431b2f0dce42a01d
zizou <zmunoz@outlook.es>
2022-09-26 17:49:17 -0400

    Removed popover code

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
-$.getJSON('https://api.mcsrvstat.us/2/mc.homienetwork.ca', function(data) {
-        
-        var playerNumber = `${data.players.online}`;
-        var serverVersion = `${data.version}`;
-        var onlineStatus = `${data.online}`;
-        
-        $("#playerno").html(playerNumber);
-        $("#serverversion").html(serverVersion);
-        if (onlineStatus == 'true'){
-            $("#serverstatus").html('<b style="color: #16aa2f;">Online</b>')
-        } else {
-            $("#serverstatus").html('Offline')
-        }
-    });
+$.getJSON('https://api.mcsrvstat.us/2/mc.homienetwork.ca', function (data) {
 
-function copyToClipboard()
-{
+    var playerNumber = `${data.players.online}`;
+    var serverVersion = `${data.version}`;
+    var onlineStatus = `${data.online}`;
+
+    $("#playerno").html(playerNumber);
+    $("#serverversion").html(serverVersion);
+    if (onlineStatus == 'true') {
+        $("#serverstatus").html('<b style="color: #16aa2f;">Online</b>')
+    } else {
+        $("#serverstatus").html('Offline')
+    }
+});
 
 
+function copyToClipboard() {
     // Copy the text inside the text field
     navigator.clipboard.writeText("mc.homienetwork.ca");
-
     // Alert the copied text
     alert("Copied: " + copyText.value);
 }
